f00e01bed0b8c0e732bcc8a5ed54341a6731a1d8
huangguanhao <huangguanhao@szboanda.net>
2018-12-13 16:00:17 +0800

    添加保留es6格式的rollup配置。
    Signed-off-by: huangguanhao <huangguanhao@szboanda.net>

---
+++ rollup.config-es.js
@@ -0,0 +1,26 @@
+// import babel from 'rollup-plugin-babel';
+
+let banner = `/*
+ * charts for WeChat small app v1.0
+ *
+ * https://github.com/xiaolin3303/wx-charts
+ * 2016-11-28
+ *
+ * Designed and built with all the love of Web
+ */
+`;
+
+export default {
+    input: 'src/app.js',
+    output: {
+        file: 'dist/wxcharts.js',//-es6
+        format: 'cjs',
+        banner: banner
+    },
+    plugins: [
+        // babel({
+        //     exclude: 'node_modules/**',
+        // })
+    ],
+
+};
